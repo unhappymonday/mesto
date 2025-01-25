0e82e5afd7fe3634d2678aa8ce1c03df6df2a937
Egor Dosekin <dos.eg1986@gmail.com>
2025-01-25 22:25:45 +0400

    added validation to user and cards schema

--- src/models/cards.ts
+++ src/models/cards.ts
@@ -1,28 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 interface ICard {
-  name: string,
-  link: string,
-  owner: mongoose.Schema.Types.ObjectId,
-  likes?: Array<mongoose.Types.ObjectId>,
-  createdAt: Date,
+  name: string;
+  link: string;
+  owner: mongoose.Schema.Types.ObjectId;
+  likes?: Array<mongoose.Types.ObjectId>;
+  createdAt: Date;
 }
 
 const cardSchema = new mongoose.Schema<ICard>({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    minlength: [2, "Минимальная длина должна составлять не менее 2 символов"],
+    maxlength: [30, "Максимальная длина не должна превышать 30 символов"],
+    required: [true, "Поле должно быть заполнено"],
   },
   link: {
     type: String,
-    required: true,
+    required: [true, "Поле должно быть заполнено"],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
+    ref: "user",
+    required: [true, "Поле должно быть заполнено"],
   },
   likes: {
     type: [mongoose.Schema.Types.ObjectId],
@@ -34,4 +34,4 @@ const cardSchema = new mongoose.Schema<ICard>({
   },
 });
 
-export default mongoose.model<ICard>('card', cardSchema);
+export default mongoose.model<ICard>("card", cardSchema);
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 interface IUser {
   name: string;
@@ -9,20 +9,20 @@ interface IUser {
 const userSchema = new mongoose.Schema<IUser>({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    minlength: [2, "Минимальная длина должна составлять не менее 2 символов"],
+    maxlength: [30, "Максимальная длина не должна превышать 30 символов"],
+    required: [true, "Поле должно быть заполнено"],
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 200,
-    required: true,
+    minlength: [2, "Минимальная длина должна составлять не менее 2 символов"],
+    maxlength: [200, "Максимальная длина не должна превышать 200 символов"],
+    required: [true, "Поле должно быть заполнено"],
   },
   avatar: {
     type: String,
-    required: true,
+    required: [true, "Поле должно быть заполнено"],
   },
 });
 
-export default mongoose.model<IUser>('user', userSchema);
+export default mongoose.model<IUser>("user", userSchema);
